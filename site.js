a8ea103e79ac3f6354fab911bc819df47e1245d2
kkyyzz <60494383+kkyyzz@users.noreply.github.com>
2020-03-11 21:42:51 +0800

    Update Contacts

--- site.js
+++ site.js
@@ -1,19 +1,19 @@
-window.addEventListener('DOMContentLoaded', () => {
+"use strict";
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-			}
-		});
-	});
+window.addEventListener('DOMContentLoaded', function () {
+  var observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      var id = entry.target.getAttribute('id');
 
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('section[id]').forEach((section) => {
-		observer.observe(section);
-	});
+      if (entry.intersectionRatio > 0) {
+        document.querySelector("nav li a[href=\"#".concat(id, "\"]")).parentElement.classList.add('active');
+      } else {
+        document.querySelector("nav li a[href=\"#".concat(id, "\"]")).parentElement.classList.remove('active');
+      }
+    });
+  }); // Track all sections that have an `id` applied
 
+  document.querySelectorAll('section[id]').forEach(function (section) {
+    observer.observe(section);
+  });
 });
